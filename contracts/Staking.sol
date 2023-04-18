dfdefa30dcfe56175180b484ae07853592f65eb7
willcordeiro <willcordeiro800@gmail.com>
2023-04-18 14:14:58 -0300

    added some logic

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -12,7 +12,6 @@ contract Staking {
     IERC20 public stakingToken;
 
     struct Position {
-        uint256 positionId;
         address walletAddress;
         uint256 createdDate;
         uint256 amountStaked;
@@ -23,18 +22,19 @@ contract Staking {
 
     Position position;
 
-    uint256 public currentPositionId;
     mapping(uint256 => Position) public positions;
     mapping(address => uint256[]) public positionIdsByAddress;
 
     constructor(address token) {
         owner = msg.sender;
-        currentPositionId = 0;
         stakingToken = IERC20(token);
     }
 
     modifier onlyOwner() {
-        require(owner == msg.sender, "Only owner may modify staking periods");
+        require(
+            owner == msg.sender,
+            "Only owner may modify an staking position"
+        );
         _;
     }
 
@@ -42,26 +42,11 @@ contract Staking {
         //add a better form to condition this function to works only with the tiers wanted for the app
         stakingToken.transferFrom(msg.sender, address(this), amount);
 
-        //TODO: CHANGE THAT
-        positions[currentPositionId] = Position(
-            currentPositionId,
+        position = Position(
             msg.sender,
-            block.timestamp,
-            block.timestamp + (numDays * 1 days),
-            tiers[numDays],
-            amount,
-            calculateInterest(tiers[numDays], amount),
-            true
+            position.amountStaked += amount,
+           //calculateInterest(tiers[numDays], amount),
         );
-
-        positionIdsByAddress[msg.sender].push(currentPositionId);
-        currentPositionId += 1;
-    }
-
-    function getPositionByID(
-        uint256 positionId
-    ) external view returns (Position memory) {
-        return positions[positionId];
     }
 
     function getPositionIdsForAddress(
@@ -72,29 +57,24 @@ contract Staking {
 
     function unstake() external onlyOwner {
         require(
-            positions[positionId].walletAddress == msg.sender,
+            position.walletAddress == msg.sender,
             "Only position creator may modify position"
         );
+
+        let amount = positions.amountStaked
 
         stakingToken.transfer(msg.sender, amount);
     }
 
     function harvest() external onlyOwner {
         require(
-            positions[positionId].walletAddress == msg.sender,
+            position[positionId].walletAddress == msg.sender,
             "Only position creator may modify position"
         );
-        require(positions[positionId].open == true, "Position is closed");
 
-        positions[positionId].open = false;
+        let amount = positions.rewardStaked
 
-        if (block.timestamp > positions[positionId].unlockDate) {
-            uint256 amount = positions[positionId].weiStaked +
-                positions[positionId].weiInterest;
-
-            stakingToken.transfer(msg.sender, amount);
-        } else {
-            stakingToken.transfer(msg.sender, positions[positionId].weiStaked);
-        }
+        stakingToken.transfer(msg.sender, amount);
+        
     }
 }
